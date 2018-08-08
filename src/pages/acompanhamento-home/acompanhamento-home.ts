22db09a210b6f853361e46113c72cedbcd6df9a8
edyonil <edyonil@gmail.com>
2018-08-08 18:46:54 -0300

    Concluído Tela de home de processo

--- src/pages/acompanhamento-home/acompanhamento-home.ts
+++ src/pages/acompanhamento-home/acompanhamento-home.ts
@@ -15,7 +15,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AcompanhamentoHomePage {
 
+  public fases: Array<any>;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+    this.fases = [
+      {
+        nome: 'Primeira fase',
+        conteudo: 'Cadastro das informações Básicas',
+        data: '23/12/2018',
+        ativo: true
+      },
+      {
+        nome: 'Segunda fase',
+        conteudo: 'Envio de suas documentações',
+        data: '24/12/2018',
+        ativo: true
+      },
+      {
+        nome: 'Terceira fase',
+        conteudo: 'Analise por parte do contador',
+        data: null,
+        ativo: false
+      },
+      {
+        nome: 'Quarta fase',
+        conteudo: 'Assinatura final do advogado',
+        data: null,
+        ativo: false
+      }
+
+    ]
   }
 
   ionViewDidLoad() {
