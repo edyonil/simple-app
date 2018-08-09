ab9a6a7474d3d3c7161928244ca0a5ec4845ced6
diegohyenna <dgmartins87@hotmail.com>
2018-08-09 13:42:12 -0300

    tela documento

--- src/pages/documento/documento.ts
+++ src/pages/documento/documento.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the DocumentoPage page.
@@ -16,8 +18,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class DocumentoPage {
 
   public doctypes = [];
+  public docForm: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public alertCtrl: AlertController,
+    public formBuilder: FormBuilder,
+  ) {
+
+    this.docForm = this.formBuilder.group({
+      docFormType: [null, [Validators.required]],
+      docFormFile: [null, [Validators.required]]
+    });
+
   }
 
   ionViewDidLoad() {
@@ -56,5 +70,24 @@ export class DocumentoPage {
 
   public onSelect(event){
     console.log(event);
+  }
+
+  public onSubmit(){
+    console.log(this.docForm.value);
+
+    let alert = this.alertCtrl.create({
+      title: 'Sucesso',
+      message: 'A foto do documento foi enviada com sucesso',
+      buttons: [
+        {
+          text: 'Fechar',
+          role: 'cancel',
+          handler: () => {
+            this.navCtrl.setRoot(HomePage)
+          }
+        },
+      ]
+    });
+    alert.present();
   }
 }
